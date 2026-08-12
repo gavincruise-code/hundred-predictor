2264ce99082709ed196f7ae2122e55fb5503aab6
Gavin <gavin@user.com>
2026-08-12 09:20:28 +0100

    chore: resolve model conflicts with daily update

--- test_fetch.js
+++ test_fetch.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-http.get('http://localhost:3002/api/live-match?url=https://www.espncricinfo.com/series/the-hundred-men-s-competition-2026-1521192/manchester-super-giants-vs-welsh-fire-21st-match-1521217/live-cricket-score', (res) => {
+http.get('http://localhost:3002/api/live-match?url=https://www.espncricinfo.com/series/the-hundred-men-s-competition-2026-1521176/birmingham-phoenix-men-vs-sunrisers-leeds-men-24th-match-1521254/live-cricket-score', (res) => {
   let data = '';
   res.on('data', (chunk) => { data += chunk; });
   res.on('end', () => {
